662a04840db5efe18de1c9dc66522ef7c6ee039e
RichZett <rz_13@live.se>
2025-05-26 12:28:34 +0100

    Implented logic LED and buttons

--- build/zephyr/isr_tables.c
+++ build/zephyr/isr_tables.c
@@ -58,13 +58,13 @@ uintptr_t __irq_vector_table _irq_vector_table[48] = {
 	((uintptr_t)&_isr_wrapper),
 };
 struct _isr_table_entry __sw_isr_table _sw_isr_table[48] = {
-	{(const void *)0x23e9, (ISR)0x4a3d}, /* 0 */
+	{(const void *)0x27b9, (ISR)0x4e7f}, /* 0 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 1 */
-	{(const void *)0x4ff0, (ISR)0x4995}, /* 2 */
+	{(const void *)0x543c, (ISR)0x4dd7}, /* 2 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 3 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 4 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 5 */
-	{(const void *)0x2a69, (ISR)0x4a3d}, /* 6 */
+	{(const void *)0x2e39, (ISR)0x4e7f}, /* 6 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 7 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 8 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 9 */
@@ -75,7 +75,7 @@ struct _isr_table_entry __sw_isr_table _sw_isr_table[48] = {
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 14 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 15 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 16 */
-	{(const void *)0x0, (ISR)0x1e41}, /* 17 */
+	{(const void *)0x0, (ISR)0x2211}, /* 17 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 18 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 19 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 20 */
--- src/main.c
+++ src/main.c
@@ -1,19 +1,35 @@
 #include <zephyr/kernel.h>          /* for k_msleep() */
 #include <zephyr/device.h>          /* for device_is_ready() and device structure */
 #include <zephyr/devicetree.h>		/* for DT_NODELABEL() */
-#include <zephyr/drivers/gpio.h>    /* for GPIO api*/
+#include <zephyr/drivers/gpio.h>    /* for GPIO api */
+#include <stdlib.h>  				/* for abs() */
+
 
 /* Define the polling time, in ms */
-#define SLEEP_TIME_MS 100
+#define SLEEP_TIME_MS 100 // Remove??
 
-/* Get led0 and button0 node IDs. Refer to the DTS file */
+/* Get leds and buttons node IDs. Refer to the DTS file */
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
+#define LED3_NODE DT_ALIAS(led3)
+
 #define BUT0_NODE DT_ALIAS(sw0)
+#define BUT1_NODE DT_ALIAS(sw1)
+#define BUT2_NODE DT_ALIAS(sw3) 
 
 /* Get the device pointer, pin number, and configuration flags for led0 and button 0. A build error on this line means your board is unsupported. */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-static const struct gpio_dt_spec but = GPIO_DT_SPEC_GET(BUT0_NODE, gpios);
+/* LEDs*/
+static const struct gpio_dt_spec led 		= GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led_normal = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec led_low  	= GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+static const struct gpio_dt_spec led_high 	= GPIO_DT_SPEC_GET(LED3_NODE, gpios);
 
+/* Buttons */
+static const struct gpio_dt_spec but 	 = GPIO_DT_SPEC_GET(BUT0_NODE, gpios);
+static const struct gpio_dt_spec but_inc = GPIO_DT_SPEC_GET(BUT1_NODE, gpios);
+static const struct gpio_dt_spec but_dec = GPIO_DT_SPEC_GET(BUT2_NODE, gpios);
+ 
 /*
  * The main function
  */
@@ -21,11 +37,17 @@ int main(void)
 {
 	int ret =0;
 	
-	/* Check if device are ready */
+	/* Check if devices are ready */
 	if (!device_is_ready(led.port) || !device_is_ready(but.port)) {
 		return 0;
 	}	
-
+	if (!device_is_ready(but_inc.port)) {
+		return 0;
+	}
+	if (!device_is_ready(but_dec.port)) {
+		return 0;
+	}
+	
 	/* Configure the GPIO pins for input/output and set active logic */
 	/* Note that the devicetree activates the internal pullup and sets the active low flag */
 	/*   so an external resistor is not needed and pressing the button causes a logic level of 1*/
@@ -33,8 +55,27 @@ int main(void)
 	if (ret < 0) {
 		return 0;
 	}
-
+	ret = gpio_pin_configure_dt(&led_normal, GPIO_OUTPUT_INACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+	ret = gpio_pin_configure_dt(&led_low, GPIO_OUTPUT_INACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+	ret = gpio_pin_configure_dt(&led_high, GPIO_OUTPUT_INACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
 	ret = gpio_pin_configure_dt(&but, GPIO_INPUT);
+	if (ret < 0) {
+		return 0;
+	}
+	ret = gpio_pin_configure_dt(&but_inc, GPIO_INPUT);
+	if (ret < 0) {
+		return 0;
+	}
+	ret = gpio_pin_configure_dt(&but_dec, GPIO_INPUT);
 	if (ret < 0) {
 		return 0;
 	}
@@ -43,24 +84,66 @@ int main(void)
 	 * The main loop
 	 */
 	bool last_butStat = false;
+	bool last_butInc = false;
+	bool last_butDec = false;
+
 	bool system_on = false;
+	int set_temp = 25; 
+	int current_temp = 25;  // simulated temp
+
 
 	while(1) {
 		bool current_butStat = gpio_pin_get_dt(&but);
+		bool current_butInc = gpio_pin_get_dt(&but_inc);
+		bool current_butDec = gpio_pin_get_dt(&but_dec);
 
-		// Detect rising edge (button pressed)
+
+		/* ON/OFF with button1(sw0) */
 		if (current_butStat && !last_butStat) {
 			system_on = !system_on;
 			printk("System toggled %s\n", system_on ? "ON" : "OFF");
 
 			// Set LED accordingly
-			gpio_pin_set_dt(&led, system_on ? 1 : 0);
+			gpio_pin_set_dt(&led, system_on ? 1 : 0); 
 
 			k_msleep(300); // debounce delay
 		}
 
+		/* Increase the set_temp with button2(sw1) */
+		if (current_butInc && !last_butInc)
+		{
+			set_temp++; 
+			printk("The set temp increased to %d\n", set_temp);
+			k_msleep(300); // debounce
+
+		}
+
+		/* Decrease the set_temp with button4(sw3)*/
+		if (current_butDec && !last_butDec) 
+		{
+		set_temp--;
+		printk("The set temp decreased to %d\n", set_temp);
+		k_msleep(300);
+		}
+
+		last_butInc = current_butInc;
+		last_butDec = current_butDec;
 		last_butStat = current_butStat;
-		k_msleep(SLEEP_TIME_MS);
+		
+		if (system_on) {
+			int diff = current_temp - set_temp;
+
+			gpio_pin_set_dt(&led_normal, (abs(diff) <= 2) ? 1 : 0);
+			gpio_pin_set_dt(&led_low,  (diff < -2) ? 1 : 0);
+			gpio_pin_set_dt(&led_high, (diff > 2) ? 1 : 0);
+		} else {
+			// All off if system is off
+			gpio_pin_set_dt(&led_normal, 0);
+			gpio_pin_set_dt(&led_low,  0);
+			gpio_pin_set_dt(&led_high, 0);
+		}
+ 
+		k_msleep(SLEEP_TIME_MS); 
 	}
 
 	return 0;
